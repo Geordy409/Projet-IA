a0d832af9514f4cf6f6f255f6a1b5e4e36e0591e
Malexia <118545402+Malexia@users.noreply.github.com>
2024-12-09 11:29:35 +0100

    ajout des images et modification des scripts

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -4,16 +4,16 @@ import json
 schemas = [
     {
         "id": 1,
-        "description": "Schéma représentant un diagramme de flux avec trois étapes : Entrée, Traitement, Sortie.",
-        "schema_type": "diagramme de flux",
-        "image_path": "data/images/flux_diagram.png"
+        "description": "Schéma représentant un diagramme du fonctionnement d'ordinateur avec trois étapes : Entrée, Traitement, Sortie.",
+        "schema_type": "diagramme d'ordinateur",
+        "image_path": "data/images/ordinateur_diagram.png"
     },
     {
         "id": 2,
         "description": "Organigramme d’une entreprise avec différents départements.",
         "schema_type": "organigramme",
-        "image_path": "data/images/organisation_chart.png"
-    },
+        "image_path": "data/images/organisation_chart.jpeg"
+    }
 ]
 
 # Sauvegarder les données dans un fichier JSON
